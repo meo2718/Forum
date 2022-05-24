1fd1a029c1c72d01cada0dadb1b850632be650b8
owner <owner@ownernoMacBook-Pro.local>
2022-05-24 19:45:11 +0900

    Linter/Formatter追加 #1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-
+ # if: -> { new_record? || changes[:crypted_password] }
+ # *登録したユーザーがパスワード以外のプロフィール項目を更新したい場合に、パスワードの入力を省略させる
   validates :email, uniqueness: true, presence: true
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
